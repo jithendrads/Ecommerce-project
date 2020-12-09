a7e1d56c85fde366d56e356989bd15c7f61da612
Jithendra <jithendrads@gmail.com>
2020-12-09 20:17:24 +0530

    integrated spring boot and angular

--- Angular-FrontEnd/angular-ecommerce/src/app/app.module.ts
+++ Angular-FrontEnd/angular-ecommerce/src/app/app.module.ts
@@ -2,15 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ProductListsComponent } from './components/product-lists/product-lists.component';
+import { HttpClientModule } from '@angular/common/http'
+import { ProductService } from './services/product.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductListsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [ProductService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
